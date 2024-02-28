4723efff74dc33c8b0e787c9b22346e64936f869
GalusGaming <kaxtom29@gmail.com>
2024-02-28 09:25:33 +0100

    1 upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+//        MENU
+        int exit =0;
+        int doSwitcha = 5;
+        while (exit == 0){
+            switch(doSwitcha) {
+                case 1:
+                    //Tworzy liste
+                    break;
+                case 2:
+                    //odznacza liste jako wykonaną
+                    break;
+                case 3:
+                    //usuwa liste
+                    break;
+                case 4:
+                    //wyświetla listę
+                    break;
+                case 5:
+//                    System.out.println("chuj");
+                        exit = 1;
+                    //wychodzi z programu
+                    break;
+
+            }
         }
     }
 }
