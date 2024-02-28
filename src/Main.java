368f5e96ad6060b0df9b83f405bf6cd1a1c08b06
galusgaming <kaxtom29@gmail.com>
2024-02-28 18:44:46 +0100

    final update with ended application. finish Main.java and ToDoList.java

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
-
+        ToDoList toDoList = new ToDoList();
 //        MENU
+        Scanner scan = new Scanner(System.in);
         int exit =0;
         int doSwitcha = 5;
         while (exit == 0){
+            System.out.println("1. Dodaj nowe zadanie\n" +
+                    "2. Oznacz zadanie jako zakończone\n" +
+                    "3. Usuń zadanie\n" +
+                    "4. Wyświetl listę zadań\n" +
+                    "5. Wyjście\n" +
+                    "\n" +
+                    "Wybierz opcję (1/2/3/4/5): ");
+            doSwitcha = scan.nextInt();
+            
             switch(doSwitcha) {
                 case 1:
+                    toDoList.addTask();
                     //Tworzy liste
                     break;
                 case 2:
+                    toDoList.markTaskAsDone();
                     //odznacza liste jako wykonaną
                     break;
                 case 3:
+                    toDoList.removeTask();
                     //usuwa liste
                     break;
                 case 4:
+                    toDoList.displayList();
                     //wyświetla listę
                     break;
                 case 5:
 //                    System.out.println("chuj");
                         exit = 1;
                     //wychodzi z programu
+                    break;
+                default:
                     break;
 
             }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,3 +1,41 @@
-public class ToDoList {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class ToDoList {
+    Scanner scan = new Scanner(System.in);
+
+    ArrayList<String> tasks = new ArrayList<String>();
+    int nr = 1;
+    public void addTask() {
+        //Tworzy liste
+        System.out.println("Podaj nazwę zadania: ");
+        String taskName = scan.nextLine();
+        System.out.println("Podaj opis zadania: ");
+        String taskDescription = scan.nextLine();
+        tasks.add(nr-1,nr+". [ ] "+taskName + " - " + taskDescription);
+        nr++;
+
+    }
+    public void markTaskAsDone() {
+        System.out.println("Podaj numer zadania, które chcesz oznaczyć jako zakończone: ");
+        int taskNumber = scan.nextInt();
+        tasks.set(taskNumber-1,tasks.get(taskNumber-1).replace("[ ]", "[x]"));
+        //odznacza liste jako wykonaną
+    }
+    public void removeTask() {
+        System.out.println("Podaj numer zadania, które chcesz usunąć: ");
+        int taskInt = scan.nextInt();
+        tasks.remove(taskInt-1);
+        //usuwa liste
+    }
+    public void displayList() {
+        if(!tasks.isEmpty()){
+        for (String task : tasks) {
+            System.out.println(task);
+            }
+        }else {
+            System.out.println("Lista jest pusta");
+        }
+        //wyświetla listę
+    }
 }
